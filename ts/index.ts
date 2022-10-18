92807310c4a87f3a7cfe48cde641ccf71f020e25
南雷 <nanlei.roy@bytedance.com>
2022-10-18 17:50:54 +0800

    feat: clean code

--- ts/index.ts
+++ ts/index.ts
@@ -1,24 +1,27 @@
-import { readFileSync } from "fs";
-import ts from "typescript";
-import _ from 'lodash';
+import { readFileSync } from 'fs'
+import ts from 'typescript'
+import _ from 'lodash'
 
 const list: Array<{
-  name: string,
+  name: string
   count: number
-}> = [];
+}> = []
+
 export function delint(sourceFile: ts.SourceFile) {
-  delintNode(sourceFile);
+  delintNode(sourceFile)
 
   function delintNode(node: ts.Node) {
     switch (node.kind) {
       case ts.SyntaxKind.Identifier:
-        const identifier = node as ts.Identifier;
+        const identifier = node as ts.Identifier
         if (identifier.parent.kind !== ts.SyntaxKind.FunctionDeclaration) {
           return
         }
-        const words: string[] = _.kebabCase(identifier.text).split('-').slice(0, 1);
+        const words: string[] = _.kebabCase(identifier.text)
+          .split('-')
+          .slice(0, 1)
         words.forEach((word) => {
-          const index = list.map(v => v.name).indexOf(word);
+          const index = list.map((v) => v.name).indexOf(word)
           // 有则改之
           if (index !== -1) {
             list[index].count++
@@ -32,22 +35,27 @@ export function delint(sourceFile: ts.SourceFile) {
         })
     }
 
-    ts.forEachChild(node, delintNode);
+    ts.forEachChild(node, delintNode)
   }
 }
 
-const fileNames = process.argv.slice(2);
-fileNames.forEach(fileName => {
+const fileNames = process.argv.slice(2)
+fileNames.forEach((fileName) => {
   // Parse a file
   const sourceFile = ts.createSourceFile(
     fileName,
     readFileSync(fileName).toString(),
     ts.ScriptTarget.ES2015,
     /*setParentNodes */ true
-  );
+  )
 
   // delint it
-  delint(sourceFile);
-  list.sort((a, b) => b.count - a.count);
-  console.log(list.slice(0, 10).map(item => item.name +"："+item.count).join('\n'));
-});
+  delint(sourceFile)
+  list.sort((a, b) => b.count - a.count)
+  console.log(
+    list
+      .slice(0, 10)
+      .map((item) => item.name + '：' + item.count)
+      .join('\n')
+  )
+})
